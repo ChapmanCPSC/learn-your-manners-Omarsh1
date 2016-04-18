b6d27ea8ac0c19d3a478dd39620332a6f3b73e6b
Omar Sherief <OmarSherief@omars-macbook-pro-2.local>
2016-04-18 08:35:00 -0700

    Initial Commit

---
+++ MannersApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MannersApplication
+//
+//  Created by Omar Sherief on 4/18/16.
+//  Copyright © 2016 Project(X). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
